4c3ae2ed39126ca5db316d3f426bb7984941cc74
Jônatas <jolimenezes@gmail.com>
2025-09-23 22:25:04 -0300

    Começo da atividade Avaliativa

---
+++ Controllers/ArmasController.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RpgApi.Data;
+using RpgApi.Models;
+
+namespace RpgApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ArmasController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ArmasController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSingle(int id)
+        {
+            try
+            {
+                Armas a = await _context.TB_ARMAS.FirstOrDefaultAsync(pBusca => pBusca.Id == id);
+
+                return Ok(a);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                List<Armas> lista = await _context.TB_ARMAS.ToListAsync();
+
+                return Ok(lista);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }       
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Armas novoArma)
+        {
+            try
+            {
+                if (novoArma.Dano > 70)
+                {
+                    throw new Exception ("O dano da arma não pode ser maior que 100");
+                }
+                await _context.TB_ARMAS.AddAsync(novoArma);
+                await _context.SaveChangesAsync();
+
+                return Ok(novoArma.Id);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Armas novoArma)
+        {
+            try
+            {
+                if (novoArma.Dano > 70)
+                {
+                    throw new Exception ("O dano da arma não pode ser maior que 100");
+                }
+                _context.TB_ARMAS.Update(novoArma);
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                Armas aRemover = await _context.TB_ARMAS.FirstOrDefaultAsync(p => p.Id == id);
+
+                _context.TB_ARMAS.Remove(aRemover);
+                int linhasAfetadas = await _context.SaveChangesAsync();
+
+                return Ok(linhasAfetadas);
+            }
+            catch (System.Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ script02_TabelaArmas.sql
@@ -0,0 +1,27 @@
+﻿BEGIN TRANSACTION;
+CREATE TABLE [TB_ARMAS] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] varchar(200) NOT NULL,
+    [Dano] int NOT NULL,
+    CONSTRAINT [PK_TB_ARMAS] PRIMARY KEY ([Id])
+);
+
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ARMAS]'))
+    SET IDENTITY_INSERT [TB_ARMAS] ON;
+INSERT INTO [TB_ARMAS] ([Id], [Dano], [Nome])
+VALUES (1, 25, 'Zangetsu'),
+(2, 10, 'Cassull & Chacal'),
+(3, 20, 'Lança Invertida do Céu'),
+(4, 35, 'Kamutoke'),
+(5, 45, 'Ragnork Demon'),
+(6, 50, 'Master Sword'),
+(7, 21, 'Ferrão Puro');
+IF EXISTS (SELECT * FROM [sys].[identity_columns] WHERE [name] IN (N'Id', N'Dano', N'Nome') AND [object_id] = OBJECT_ID(N'[TB_ARMAS]'))
+    SET IDENTITY_INSERT [TB_ARMAS] OFF;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250924010903_MigracaoArma', N'9.0.9');
+
+COMMIT;
+GO
+
